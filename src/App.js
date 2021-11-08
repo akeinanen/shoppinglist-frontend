7a8d45559f168af643a54a842d30f32cd4f00612
Akseli Keinänen <keinanenakseli@gmail.com>
2021-11-08 12:51:55 +0200

    Push project to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './app.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const URL = "http://localhost/shoppinglist/";
+
+  useEffect(() => {
+    axios.get(URL)
+      .then(response => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, [])
+
+  const addItem = (e) => {
+    e.preventDefault();
+    if (!description || !amount) return;
+    const json = JSON.stringify({ description: description, amount: amount });
+    console.log(json)
+    axios.post(
+      URL + 'add.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setDescription('');
+        setAmount('');
+      }).catch(error => {
+        alert(error.response.data.error);
+      });
+  }
+
+  const removeItem = (id) => {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + "delete.php", json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        setItems(items.filter(item => item.id !== id));
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <h3>Shopping list</h3>
+      <form onSubmit={addItem}>
+        <label>New item</label>
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <button>Add</button>
+      </form>
+
+      <table>
+        <tbody>
+          {items?.map(item => (
+            <tr key={item.id}>
+              <td>{item.description}</td>
+              <td>{item.amount}</td>
+              <td className="remove" onClick={() => removeItem(item.id)}>remove</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
